73fac88d51da3df13f9bcfa0f032a1a32cc3e7a4
GiovanniCst <g.costantini@gmail.com>
2025-08-02 14:34:45 +0200

    Feature: Add ball hit sound effect to bricks with snappy disappearance

--- scripts/brick.gd
+++ scripts/brick.gd
@@ -1,4 +1,20 @@
 extends StaticBody2D
 
+@onready var audio_player = $AudioStreamPlayer2D
+
 func hit():
+	print("Brick hit! Playing sound...")
+	if is_instance_valid(audio_player):
+		audio_player.play()
+		print("Sound played!")
+	else:
+		print("Audio player not valid!")
+	
+	# Hide the brick immediately for snappy disappearance
+	visible = false
+	set_process(false)
+	set_physics_process(false)
+	
+	# Queue free after a short delay to allow sound to play
+	await get_tree().create_timer(0.2).timeout # Adjust delay as needed
 	queue_free()
